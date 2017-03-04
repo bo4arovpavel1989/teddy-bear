ec169bc765a7c21a876f921f98415c345723c051
bo4arovpavel1989 <bo4arovpavel1989@mail.ru>
2017-03-04 13:50:05 +0300

    made calculation of price for the cart

---
+++ public/scripts/cart.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+	deleteProductFromCart();
+	calculateTotalPrice();
+});
+
+function openPrevSection(button){
+		button.parent().addClass('hidden');
+		button.parent().prev().removeClass('active');
+		button.parent().prev().prev().removeClass('hidden');
+		button.parent().prev().prev().prev().addClass('active');
+}
+
+function openNextSection(button){
+		button.parent().addClass('hidden');
+		button.parent().prev().removeClass('active');
+		button.parent().next().addClass('active');
+		button.parent().next().next().removeClass('hidden');
+}
+
+function recalculatePrice(priceField) {
+	var price = priceField.parent().prev().data('price');
+	var newPrice = Number(price) * priceField.val();
+	priceField.parent().next().children('.sumPrice').html(newPrice);
+	calculateTotalPrice()
+}
+
+function deleteProductFromCart(){
+	$('.deleteProductFromCart').on('click', function(e){
+		$(this).parent().parent().remove();
+		calculateTotalPrice();
+		e.preventDefault();
+	});
+}
+
+function calculateTotalPrice(){
+	var totalPrice = new Number;
+	var price;
+	$('.sumPrice').each(function(){
+		price = $(this).html();
+		price = parseInt(price);
+		totalPrice += price;
+	});
+	$('#totalPrice').html(totalPrice + ' руб.');
+};
