17abca85aba71feaaf8ee952b4aa1371c3779c02
bo4arovpavel1989 <bo4arovpavel1989@mail.ru>
2017-03-09 20:30:03 +0300

    made orders section in admin. all handlers done

---
+++ public/scripts/order.js
@@ -0,0 +1,122 @@
+$(document).ready(function(){
+	getOrders();
+	orderArchive();
+	backToOrders();
+});
+
+function getOrders(){
+	$.ajax({
+		url: '/admin/getorders',
+		dataType: 'html',
+		success: function(data) {
+			$('#orderList').empty();
+			$('#orderList').append(data);
+			showInfo();
+			orderToWork();
+			doneOrder();
+			deleteOrder();
+		}
+	});
+}
+
+function showInfo(){
+	$('.showInfo').on('click', function(){
+		$(this).parent().next().toggleClass('hidden');
+		return false;
+	});
+}
+
+function orderToWork() {
+	$('.markSeenOrder').on('click', function(){
+		var orderId = $(this).data('id');
+		var query = '/admin/ordertowork?order=' + orderId;
+		$.ajax({
+				url: query,
+				type: 'post',
+				dataType: 'html', 
+				success: function(){
+					getOrders();
+				}
+			});
+		return false;	
+	});
+}
+
+function doneOrder(){
+	$('.markDoneOrder').on('click', function(){
+		var answer = confirm('Заказ отработан?');
+		if (answer) {
+			var orderId = $(this).data('id');
+			var query='/admin/markdoneorder?order=' + orderId;
+			$.ajax({
+					url: query,
+					type: 'post',
+					dataType: 'html',
+					success: function(){
+						getOrders();
+					}
+			});
+		}
+		return false;
+	});
+}
+
+function deleteOrder(){
+	$('.deleteOrder').on('click', function(){
+		var answer = confirm('Уверен?');
+		if (answer) {
+			var answer2 = confirm(' Точно уверен?');
+			if (answer2) {
+			var orderId = $(this).data('id');
+			var query='/admin/deleteorder?order=' + orderId;
+			$.ajax({
+					url: query,
+					type: 'delete',
+					dataType: 'html',
+					success: function(){
+						if($('#orderArchive').hasClass('hidden')) /*made this to return to the exact page user was (actual orders or archive)*/
+							$.ajax({
+								url: '/admin/getorderarchive',
+								dataType: 'html',
+								success: function(data) {
+										$('#orderList').empty();
+										$('#orderList').append(data);
+										showInfo();
+										deleteOrder();
+								}
+							});
+						else getOrders();
+					}
+			});
+			}
+		}
+		return false;
+	});
+}
+
+function orderArchive(){
+	$('#orderArchive').on('click', function(){
+		$(this).toggleClass('hidden');
+		$('#backToOrders').toggleClass('hidden');
+		$.ajax({
+			url: '/admin/getorderarchive',
+			dataType: 'html',
+			success: function(data) {
+				$('#orderList').empty();
+				$('#orderList').append(data);
+				showInfo();
+				deleteOrder();
+			}
+		});
+		return false;
+	});
+}
+
+function backToOrders(){
+	$('#backToOrders').on('click', function(){
+		$(this).toggleClass('hidden');
+		$('#orderArchive').toggleClass('hidden');
+		getOrders();
+		return false;
+	});
+}
