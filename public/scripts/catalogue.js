3a84b49cf4ceeff8493307f9fe55c1dfe7e78a84
Unknown <bo4arovpavel1989@mail.ru>
2018-01-10 09:32:12 +0300

    added translit function for address

--- public/scripts/catalogue.js
+++ public/scripts/catalogue.js
@@ -76,6 +76,7 @@ function getProductAddForm(){
 			addProductSubmit();
 			showAddProductForm();
 			closeForm();
+			translitName();
 		}
 	});
 }
@@ -92,6 +93,19 @@ function getSubCategories() {
 				$('#productAddSubcategory').append(html);
 			}
 		});
+	});
+}
+
+function translitName(){
+	$('#inputName').on('blur',function(){
+		var russianName = $('#inputName').val();
+		var dictionary={'а':'a', 'б':'b', 'в':'v', 'г':'g', 'д':'d', 'е':'e', 'ж':'g', 'з':'z', 'и':'i', 'й':'y', 'к':'k', 'л':'l', 'м':'m', 'н':'n', 'о':'o', 'п':'p', 'р':'r', 'с':'s', 'т':'t', 'у':'u', 'ф':'f', 'ы':'i', 'э':'e', 'А':'A', 'Б':'B', 'В':'V', 'Г':'G', 'Д':'D', 'Е':'E', 'Ж':'G', 'З':'Z', 'И':'I', 'Й':'Y', 'К':'K', 'Л':'L', 'М':'M', 'Н':'N', 'О':'O', 'П':'P', 'Р':'R', 'С':'S', 'Т':'T', 'У':'U', 'Ф':'F', 'Ы':'I', 'Э':'E', 'ё':'yo', 'х':'h', 'ц':'ts', 'ч':'ch', 'ш':'sh', 'щ':'shch', 'ъ':'', 'ь':'', 'ю':'yu', 'я':'ya', 'Ё':'YO', 'Х':'H', 'Ц':'TS', 'Ч':'CH', 'Ш':'SH', 'Щ':'SHCH', 'Ъ':'', 'Ь':'','Ю':'YU', 'Я':'YA'};
+		var translitName = russianName.replace(/[\s\S]/g, function(x){
+	    if( dictionary.hasOwnProperty( x ) )
+	      return dictionary[ x ];
+			return x;
+		});
+		$('#inputAddress').val(translitName);
 	});
 }
 
